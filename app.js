e5f43b012462d8075548914d6ef02726f69a4feb
Jaime Guillen <guns-jimi@hotmail.com>
2021-07-06 22:06:55 -0500

    agregar proyecto mercadito

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { dirname } = require('path');
+const path = require('path');
+const app= express();
+
+const publicPath= path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(process.env.Port || 3000, function(){
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, './views/home.html'));
+})
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, './views/login.html'));
+})
+app.get('/registro', (req, res)=>{
+    res.sendFile(path.join(__dirname, './views/register.html'));
+})
